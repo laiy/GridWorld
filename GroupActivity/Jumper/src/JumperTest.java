0a55c634c3924850c8f584a015c380a64b74731b
laiy <ly.franky@gmail.com>
2015-08-21 11:53:17 +0800

    fucking unit test

--- GroupActivity/Jumper/src/JumperTest.java
+++ GroupActivity/Jumper/src/JumperTest.java
@@ -4,6 +4,11 @@ import info.gridworld.grid.Location;
 import info.gridworld.actor.ActorWorld;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.actor.Bug;
+
+
 
 
 public class JumperTest {
@@ -22,6 +27,58 @@ public class JumperTest {
         jumper.act();
         assertEquals(45, jumper.getDirection());
         assertEquals(new Location(0, 0), jumper.getLocation());
+    }
+
+    @Test
+    public void testcase2() {
+        jumper.moveTo(new Location(0, 0));
+        jumper.setDirection(90);
+        Flower f = new Flower();
+        world.add(new Location(0, 2), f);
+        jumper.act();
+        assertEquals(135, jumper.getDirection());
+        assertEquals(new Location(0, 0), jumper.getLocation());
+        f.removeSelfFromGrid();
+        jumper.setDirection(90);
+        Rock r = new Rock();
+        world.add(new Location(0, 2), r);
+        jumper.act();
+        assertEquals(135, jumper.getDirection());
+        assertEquals(new Location(0, 0), jumper.getLocation());
+        r.removeSelfFromGrid();
+    }
+
+    @Test
+    public void testcase3() {
+        jumper.moveTo(new Location(0, 0));
+        jumper.setDirection(0);
+        jumper.act();
+        assertEquals(45, jumper.getDirection());
+        assertEquals(new Location(0, 0), jumper.getLocation());
+    }
+
+    @Test
+    public void testcase4() {
+        jumper.moveTo(new Location(0, 0));
+        jumper.setDirection(90);
+        Bug b = new Bug();
+        world.add(new Location(0, 2), b);
+        jumper.act();
+        assertEquals(135, jumper.getDirection());
+        assertEquals(new Location(0, 0), jumper.getLocation());
+        b.removeSelfFromGrid();
+    }
+
+    @Test
+    public void testcase5() {
+        jumper.moveTo(new Location(0, 0));
+        jumper.setDirection(90);
+        Bug j = new Jumper();
+        world.add(new Location(0, 2), j);
+        jumper.act();
+        assertEquals(135, jumper.getDirection());
+        assertEquals(new Location(0, 0), jumper.getLocation());
+        j.removeSelfFromGrid();
     }
 }
 
