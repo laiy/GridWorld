e3de11f8754fd526a201d4fb700b3d8e287528ae
laiy <ly.franky@gmail.com>
2015-08-22 16:28:15 +0800

    hahahahaha

--- ImageProcessing/ImageProcessor.java
+++ ImageProcessing/ImageProcessor.java
@@ -2,7 +2,6 @@ import imagereader.IImageProcessor;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
-
 public class ImageProcessor implements IImageProcessor {
 
     public Image showChanelR(Image image) {
